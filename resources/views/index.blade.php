39fbfe14b4b4c029e03e9b50b4eb15cb8ecad3ed
Alyaa Salsabel <alyaasalsabel@student.uns.ac.id>
2021-05-31 11:26:39 +0700

    bootstrap, login, regis

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,39 +1,53 @@
+@extends('layouts.app')
+@section('content')
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Klinik</title>
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
 </head>
 
 <body>
-    <div class="wrapper">
+    <div class="container">
         <h1>App Klinik</h1>
-        <h2>Daftar Tabel</h2>
-        <ul>
-            <li>
-                <a href="/perawat">Perawat</a>
-            </li>
-            <li>
-                <a href="/kunjungan">Kunjungan</a>
-            </li>
-            <li>
-                <a href="/kunjungan_poli">Kunjungan Poli</a>
-            </li>
-            <li>
-                <a href="/tindakan">Tindakan</a>
-            </li>
-            <li>
-                <a href="/bhp">BHP</a>
-            </li>
-            <li>
-                <a href="/obat">Obat</a>
-            </li>
-        </ul>
-    </div>
+            <div class="row-justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">{{ __('Daftar Tabel') }}</div>
+
+                        <div class="card-body">
+                            <ul>
+                                <li>
+                                    <a href="/perawat">Perawat</a>
+                                </li>
+                                <li>
+                                    <a href="/kunjungan">Kunjungan</a>
+                                </li>
+                                <li>
+                                    <a href="/kunjungan_poli">Kunjungan Poli</a>
+                                </li>
+                                <li>
+                                    <a href="/tindakan">Tindakan</a>
+                                </li>
+                                <li>
+                                    <a href="/bhp">BHP</a>
+                                </li>
+                                <li>
+                                    <a href="/obat">Obat</a>
+                                </li>
+                            </ul>
+                        </div>
+                            
+                    </div>
+                </div>
+            </div>
+    </div> 
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
 </body>
 
 </html>
